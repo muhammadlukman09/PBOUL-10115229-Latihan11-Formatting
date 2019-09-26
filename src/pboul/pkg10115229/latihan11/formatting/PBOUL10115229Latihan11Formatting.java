5651cae44446cf9d6037b2d1ca6cbda200a687c0
fakhri ramdani <muhammadlukman09@gmail.com>
2019-09-26 10:17:38 +0700

    project kesebelas

---
+++ src/pboul/pkg10115229/latihan11/formatting/PBOUL10115229Latihan11Formatting.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboul.pkg10115229.latihan11.formatting;
+
+/**
+ *
+ * @author Lukman
+ * NIM          : 10115229
+ * NAMA         : MUHAMMAD LUKMAN HAKIM
+ * HARITANGGAL  : SABTU, 21 SEPTEMBER 2019
+ * LOG
+ */
+
+public class PBOUL10115229Latihan11Formatting {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int i = 3456764;
+        int iMinus =-i;
+        
+        System.out.println("i : " + i);
+        System.out.printf("%%d : %d%n", i);
+        System.out.printf("%%10d : %10d%n", i);
+        System.out.printf("%%+10d : %+10d%n", i);
+        System.out.printf("%%+10d : %+10d%n", iMinus);
+        System.out.printf("%%,10d : %,10d%n", i);
+        System.out.printf("%%-10d : %-10d%n", i);
+        double f = 5675482.892;
+        System.out.println("f : " + f);
+        System.out.printf("%%f : %f%n", f);
+        System.out.printf("%%.2f : %.2f%n", f);
+        System.out.printf("%%12.2f : %12.2f%n", f);
+        System.out.printf("%%,12.2f : %,12.2f%n", f);
+    }
+    
+}
